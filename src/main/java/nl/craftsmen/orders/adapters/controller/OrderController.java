e695446ac36809b548dac835d93c737d7f3bd712
Michel Schudel <michel.schudel@gmail.com>
2026-03-16 20:31:34 +0100

    move business logic from controller to service

--- src/main/java/nl/craftsmen/orders/adapters/controller/OrderController.java
+++ src/main/java/nl/craftsmen/orders/adapters/controller/OrderController.java
@@ -5,6 +5,7 @@ import nl.craftsmen.orders.adapters.repositories.OrderRepository;
 import nl.craftsmen.orders.adapters.restclient.StockInformation;
 import nl.craftsmen.orders.adapters.restclient.StockRequest;
 import nl.craftsmen.orders.application.OrderService;
+import org.jspecify.annotations.NonNull;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
@@ -18,61 +19,27 @@ import static nl.craftsmen.orders.adapters.restclient.StockAvailabilityType.CODE
 @RequestMapping("/orders")
 public class OrderController {
 
-    private final OrderRepository repository;
-    private final RestTemplate restTemplate = new RestTemplate();
-
-    @Value("${stock.base-url}")
-    private String stockBaseUrl;
+    private final OrderService service;
 
     public OrderController(
-            OrderService service,
-            OrderRepository repository
+            OrderService service
     ) {
-        this.repository = repository;
+        this.service = service;
     }
 
     @PostMapping
     public OrderEntity placeOrder(@RequestParam String productId,
                                   @RequestParam int quantity) {
 
-        if (quantity <= 0) {
-            throw new IllegalArgumentException("Quantity must be positive");
-        }
-
-
-        if (quantity > 100) {
-            throw new IllegalArgumentException("Quantity cannot be bigger than 100");
-        }
-
-        StockInformation inStock = restTemplate.postForObject(
-                stockBaseUrl + "/api/stock/" + productId,
-                new StockRequest(
-                        productId,
-                        quantity
-                ),
-                StockInformation.class
-        );
-
-        if (inStock == null || inStock.stockAvailability() != CODE0) {
-            throw new RuntimeException("Product not in stock");
-        }
-
-        OrderEntity order = new OrderEntity();
-        order.setProductId(productId);
-        order.setQuantity(quantity);
-        order.setTotalPrice(BigDecimal.valueOf(quantity * 10));
-        order.setStatus("OPEN");
-
-        OrderEntity saved = repository.save(order);
-
-        saved.setStatus("CREATED");
+        OrderEntity saved = service.createOrder(productId, quantity);
         return saved;
 
     }
 
+
     @GetMapping
     public List<OrderEntity> getAllOrders() {
-        return repository.findAll();
+        return service.getAllOrders();
     }
 
 }
--- src/main/java/nl/craftsmen/orders/application/OrderService.java
+++ src/main/java/nl/craftsmen/orders/application/OrderService.java
@@ -1,9 +1,17 @@
 package nl.craftsmen.orders.application;
 
+import nl.craftsmen.orders.adapters.repositories.OrderEntity;
 import nl.craftsmen.orders.adapters.repositories.OrderRepository;
+import nl.craftsmen.orders.adapters.restclient.StockInformation;
+import nl.craftsmen.orders.adapters.restclient.StockRequest;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import static nl.craftsmen.orders.adapters.restclient.StockAvailabilityType.CODE0;
 
 @Service
 public class OrderService {
@@ -19,4 +27,43 @@ public class OrderService {
         this.repository = repository;
     }
 
+    public OrderEntity createOrder(String productId, int quantity) {
+        if (quantity <= 0) {
+            throw new IllegalArgumentException("Quantity must be positive");
+        }
+
+
+        if (quantity > 100) {
+            throw new IllegalArgumentException("Quantity cannot be bigger than 100");
+        }
+
+        StockInformation inStock = restTemplate.postForObject(
+                stockBaseUrl + "/api/stock/" + productId,
+                new StockRequest(
+                        productId,
+                        quantity
+                ),
+                StockInformation.class
+        );
+
+        if (inStock == null || inStock.stockAvailability() != CODE0) {
+            throw new RuntimeException("Product not in stock");
+        }
+
+        OrderEntity order = new OrderEntity();
+        order.setProductId(productId);
+        order.setQuantity(quantity);
+        order.setTotalPrice(BigDecimal.valueOf(quantity * 10));
+        order.setStatus("OPEN");
+
+        OrderEntity saved = repository.save(order);
+
+        saved.setStatus("CREATED");
+        return saved;
+    }
+
+
+    public List<OrderEntity> getAllOrders() {
+        return repository.findAll();
+    }
 }
